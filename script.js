ebb678a51ec26d7eb9d7168032691cef0fd83970
Matthew Sherman <matt5064@yahoo.com>
2021-11-17 08:25:18 -0700

    initial

---
+++ script.js
@@ -0,0 +1,22 @@
+
+function increase(){
+    var num = document.getElementById("number").innerText
+    num = parseInt(num) + 1;
+    document.getElementById("number").innerText = num;
+}
+
+function increase2(){
+    var num = document.getElementById("number2").innerText
+    num = parseInt(num) + 1;
+    document.getElementById("number2").innerText = num;
+}
+
+function increase1(){
+    var num = document.getElementById("number1").innerText
+    num = parseInt(num) + 1;
+    document.getElementById("number1").innerText = num;
+}
+
+
+
+
